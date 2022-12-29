90113b86c207ee78443d06c8695bf82eb17bc7bc
Thoreuxyohan <yohan.thoreux972@gmail.com>
2022-12-29 16:44:40 +0100

    maj Yohan

--- setup.py
+++ setup.py
@@ -4,3 +4,19 @@ import pygame, sys
 pygame.init()
 clock = pygame.time.Clock()
 
+#Setting up the main screen
+screen_width = 1280
+screen_height = 900
+screen = pygame.display.set_mode((screen_width,screen_height))
+pygame.display.set_caption('Pong')
+
+while True:
+    #Handling input
+    for event in pygame.event.get():
+        if event.type == event.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    
+    pygame.display.flip()
+    clock.tick(60)
