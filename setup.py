6b1079f4eca3600273198e26c1e7276367503d44
Thoreuxyohan <yohan.thoreux972@gmail.com>
2022-12-29 20:56:53 +0100

    Mise à jour des rebonds + Desing + Ball

--- setup.py
+++ setup.py
@@ -10,13 +10,42 @@ screen_height = 900
 screen = pygame.display.set_mode((screen_width,screen_height))
 pygame.display.set_caption('Pong')
 
+#Game Rectangles
+ball = pygame.Rect(screen_width/2 - 15,screen_height/2 - 15,30,30,)
+player = pygame.Rect(screen_width - 20,screen_height/2 - 70,10,140)
+opponent = pygame.Rect(10,screen_width/2 - 70,10,140)
+
+bg_color = pygame.Color('grey12')
+light_grey = (200,200,200)
+
+ball_speed_x = 7
+ball_speed_y = 7
+
+
+
 while True:
     #Handling input
     for event in pygame.event.get():
-        if event.type == event.QUIT:
+        if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-    
+    #Speed of the ball
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y  
+
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *=-1
+    if ball.left <= 0 or ball.right >= screen_width:
+        ball_speed_x *= -1 
+        
+    #Visual of the game
+    screen.fill(bg_color)
+    pygame.draw.rect(screen,light_grey,player)
+    pygame.draw.rect(screen,light_grey,opponent)
+    pygame.draw.ellipse(screen,light_grey,ball) 
+    pygame.draw.aaline(screen,light_grey, (screen_width/2,20), (screen_width/2,screen_height))
+
+
     pygame.display.flip()
     clock.tick(60)
